da57e93dc296af8bb45eb1d1618c17fe03ff2c1f
Moises Macero <mechero@users.noreply.github.com>
2021-10-08 10:46:08 +0200

    PracticalAdvice is now a record

--- src/main/java/io/tpd/kafkaexample/PracticalAdvice.java
+++ src/main/java/io/tpd/kafkaexample/PracticalAdvice.java
@@ -2,29 +2,6 @@ package io.tpd.kafkaexample;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class PracticalAdvice {
-    private final String message;
-    private final int identifier;
-
-    public PracticalAdvice(@JsonProperty("message") final String message,
-                           @JsonProperty("identifier") final int identifier) {
-        this.message = message;
-        this.identifier = identifier;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public int getIdentifier() {
-        return identifier;
-    }
-
-    @Override
-    public String toString() {
-        return "PracticalAdvice::toString() {" +
-                "message='" + message + '\'' +
-                ", identifier=" + identifier +
-                '}';
-    }
+record PracticalAdvice(@JsonProperty("message") String message,
+                       @JsonProperty("identifier") int identifier) {
 }
